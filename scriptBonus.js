a749ab5a4b1bf87d367020ba2ffda2b82789a329
Marco <mcg.tampieri@gmail.com>
2018-10-22 15:34:44 +0200

    Bataille fine tuning 1

--- scriptBonus.js
+++ scriptBonus.js
@@ -2,8 +2,6 @@ let bottone = document.querySelector("#start");
 
 function battle() {
 
-    let deck = [];
-
     class Card {
         constructor(name, value) {
             this.name = name;
@@ -11,68 +9,61 @@ function battle() {
         }
     }
 
-    let card1 = new Card("Ace of Spades", 1);
-    let card2 = new Card("Ace of Hearts", 1);
-    let card3 = new Card("Ace of Clubs", 1);
-    let card4 = new Card("Ace of Diamonds", 1);
-    let card5 = new Card("Two of Spades", 2);
-    let card6 = new Card("Two of Hearts", 2);
-    let card7 = new Card("Two of Clubs", 2);
-    let card8 = new Card("Two of Diamonds", 2);
-    let card9 = new Card("Three of Spades", 3);
-    let card10 = new Card("Three of Hearts", 3);
-    let card11 = new Card("Three of Clubs", 3);
-    let card12 = new Card("Three of Diamonds", 3);
-    let card13 = new Card("Four of Spades", 4);
-    let card14 = new Card("Four of Hearts", 4);
-    let card15 = new Card("Four of Clubs", 4);
-    let card16 = new Card("Four of Diamonds", 4);
-    let card17 = new Card("Five of Spades", 5);
-    let card18 = new Card("Five of Hearts", 5);
-    let card19 = new Card("Five of Clubs", 5);
-    let card20 = new Card("Five of Diamonds", 5);
-    let card21 = new Card("Six of Spades", 6);
-    let card22 = new Card("Six of Hearts", 6);
-    let card23 = new Card("Six of Clubs", 6);
-    let card24 = new Card("Six of Diamonds", 6);
-    let card25 = new Card("Seven of Spades", 7);
-    let card26 = new Card("Seven of Hearts", 7);
-    let card27 = new Card("Seven of Clubs", 7);
-    let card28 = new Card("Seven of Diamonds", 7);
-    let card29 = new Card("Eight of Spades", 8);
-    let card30 = new Card("Eight of Hearts", 8);
-    let card31 = new Card("Eight of Clubs", 8);
-    let card32 = new Card("Eight of Diamonds", 8);
-    let card33 = new Card("Nine of Spades", 9);
-    let card34 = new Card("Nine of Hearts", 9);
-    let card35 = new Card("Nine of Clubs", 9);
-    let card36 = new Card("Nine of Diamonds", 9);
-    let card37 = new Card("Ten of Spades", 10);
-    let card38 = new Card("Ten of Hearts", 10);
-    let card39 = new Card("Ten of Clubs", 10);
-    let card40 = new Card("Ten of Diamonds", 10);
-    let card41 = new Card("Jack of Spades", 11);
-    let card42 = new Card("Jack of Hearts", 11);
-    let card43 = new Card("Jack of Clubs", 11);
-    let card44 = new Card("Jack of Diamonds", 11);
-    let card45 = new Card("Queen of Spades", 12);
-    let card46 = new Card("Queen of Hearts", 12);
-    let card47 = new Card("Queen of Clubs", 12);
-    let card48 = new Card("Queen of Diamonds", 12);
-    let card49 = new Card("King of Spades", 13);
-    let card50 = new Card("King of Hearts", 13);
-    let card51 = new Card("King of Clubs", 13);
-    let card52 = new Card("King of Diamonds", 13);
+    let deck = [
+        new Card("Ace of Spades", 1),
+        new Card("Ace of Hearts", 1),
+        new Card("Ace of Clubs", 1),
+        new Card("Ace of Diamonds", 1),
+        new Card("Two of Spades", 2),
+        new Card("Two of Hearts", 2),
+        new Card("Two of Clubs", 2),
+        new Card("Two of Diamonds", 2),
+        new Card("Three of Spades", 3),
+        new Card("Three of Hearts", 3),
+        new Card("Three of Clubs", 3),
+        new Card("Three of Diamonds", 3),
+        new Card("Four of Spades", 4),
+        new Card("Four of Hearts", 4),
+        new Card("Four of Clubs", 4),
+        new Card("Four of Diamonds", 4),
+        new Card("Five of Spades", 5),
+        new Card("Five of Hearts", 5),
+        new Card("Five of Clubs", 5),
+        new Card("Five of Diamonds", 5),
+        new Card("Six of Spades", 6),
+        new Card("Six of Hearts", 6),
+        new Card("Six of Clubs", 6),
+        new Card("Six of Diamonds", 6),
+        new Card("Seven of Spades", 7),
+        new Card("Seven of Hearts", 7),
+        new Card("Seven of Clubs", 7),
+        new Card("Seven of Diamonds", 7),
+        new Card("Eight of Spades", 8),
+        new Card("Eight of Hearts", 8),
+        new Card("Eight of Clubs", 8),
+        new Card("Eight of Diamonds", 8),
+        new Card("Nine of Spades", 9),
+        new Card("Nine of Hearts", 9),
+        new Card("Nine of Clubs", 9),
+        new Card("Nine of Diamonds", 9),
+        new Card("Ten of Spades", 10),
+        new Card("Ten of Hearts", 10),
+        new Card("Ten of Clubs", 10),
+        new Card("Ten of Diamonds", 10),
+        new Card("Jack of Spades", 11),
+        new Card("Jack of Hearts", 11),
+        new Card("Jack of Clubs", 11),
+        new Card("Jack of Diamonds", 11),
+        new Card("Queen of Spades", 12),
+        new Card("Queen of Hearts", 12),
+        new Card("Queen of Clubs", 12),
+        new Card("Queen of Diamonds", 12),
+        new Card("King of Spades", 13),
+        new Card("King of Hearts", 13),
+        new Card("King of Clubs", 13),
+        new Card("King of Diamonds", 13),
+    ];
 
-    //for (i = 1; i < 53; i++) {
-    //deck.push('card' + i);
-    //}
-
-    deck.push(card1, card10, card11, card12, card13, card14, card15, card16, card17, card18, card19,
-        card2, card20, card21, card22, card23, card24, card25, card26, card27, card28, card29,
-        card3, card30, card31, card32, card33, card34, card35, card36, card37, card38, card39,
-        card4, card40, card41, card42, card43, card44, card45, card46, card47, card48, card49,
-        card5, card50, card51, card52, card6, card7, card8, card9);
     console.log(deck);
 
     alert("Shuffling deck.");
